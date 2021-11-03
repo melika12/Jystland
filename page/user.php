af9618c20d5034e166402c4af5e0ec38a40d2f0b
Melika Pedersen <melika.pedersen@r2p.com>
2021-11-03 10:21:28 +0100

    added users to user overview

--- page/user.php
+++ page/user.php
@@ -1,12 +1,37 @@
+<?php
+    require('menu.php');
+    include_once('../dbconnect.php')
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="user.css">
+        <link rel="stylesheet" href="../style/user.css">
     </head> 
-
     <body>
-
-       
-
+        <button class="newUtem">New user</button>
+        <div class="overviewUtem">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>IsAdmin</th>
+                    <th>Created</th>
+                    <th>Modified</th>
+                </tr>
+                    <?php 
+                        $users = "SELECT * FROM Users;";
+                        $result = mysqli_query($conn, $users);
+                        foreach ($result AS $user) {
+                            echo '<tr>';
+                            echo '<td>' . utf8_encode($user['Name']) . '</td>';                    
+                            echo '<td>' . utf8_encode($user['Username']) . '</td>';                    
+                            echo '<td>' . utf8_encode($user['IsAdmin']) . '</td>';                    
+                            echo '<td>' . utf8_encode($user['CreatedDate']) . '</td>';                    
+                            echo '<td>' . utf8_encode($user['ModifiedDate']) . '</td>';       
+                            echo '</tr>';      
+                        } 
+                    ?>
+            </table>
+        </div>
     </body>
 </html>
