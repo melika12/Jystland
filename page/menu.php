5144b61b6342e871898b041642dfcc96944f531c
sacha <sachafanefjord@gmail.com>
2021-11-03 10:02:22 +0100

    frontEndDone

--- page/menu.php
+++ page/menu.php
@@ -5,12 +5,13 @@
     </head> 
     <body>
         <nav>
+            <a class="navButton" href="overview.php">Home</a>
             <a class="navButton" href="placement.php">Placement's</a>
             <a class="navButton" href="statistic.php">Statistic's</a>
             <a class="navButton" href="category.php">Categorys</a>
             <a class="navButton" href="lowItem.php">Low item's</a>
             <a class="navButton" href="user.php">Users</a>
-            <a class="navButton" href="#">my user</a>
+            <a class="navButton" href="myUser.php">My user</a>
             <a class="navButton" href="#" style="margin-right: 5vw;">Logout</a>
         </nav>
     </body>
--- page/statistic.php
+++ page/statistic.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
+<?php
+    require('menu.php');
+?>
 <html>
     <head>
-        <link rel="stylesheet" href="statistic.css">
+        <link rel="stylesheet" href="../style/statistic.css">
     </head> 
 
     <body>
-
-       
-
+        <canvas id="myCanvas">
+            Your browser does not support the HTML canvas tag.
+        </canvas>
+        <canvas id="myCanvas">
+            Your browser does not support the HTML canvas tag.
+        </canvas>
+        <canvas id="myCanvas">
+            Your browser does not support the HTML canvas tag.
+        </canvas>
     </body>
 </html>
+<script>
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.strokeText("Hello World",10,50);
+</script>
