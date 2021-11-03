1e70bc71ef9d9f9f657f9aeb5a39e74ff419148c
Marcus Nielsen <mni@mjolner.dk>
2021-11-03 10:59:58 +0100

    made registration page and validation for username

--- page/login.php
+++ page/login.php
@@ -1,3 +1,18 @@
+<?php
+//Define variables and initialize with empty value
+$userpassword = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    //Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,7 +30,7 @@
                 <label style="font-size: 2vh;">
                     Remember me
                     <input style="margin-top: 5vh;font-size: 5vh; margin-top: 1vh;" type="checkbox" checked="checked" name="remember"> 
-                </label><br>                 
+                </label><br>
                 <span class="psw" style="font-size: 2vh;">Forgot <a href="#">password?</a></span>
                 <button type="submit" style="float: right;margin-right: 9vw;margin-top: -3vh;">Login</button><br>
         </form> 
