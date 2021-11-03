78958864f2b337f9b3ebebcc817e4630356b4831
Marcus Nielsen <mni@mjolner.dk>
2021-11-03 14:20:00 +0100

    commit so the project can be moved

--- page/login.php
+++ page/login.php
@@ -1,84 +1,83 @@
 <?php
+ 
+require('C:\laragon\www\Jystland\dbconnect.php');
 
-//checks if user is already loggind in, if yes then redirect them to front page
+// Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: overview.php");
     exit;
 }
-//Define variables and and initialize with empty values
+
+// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
-
-
+ 
+// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Checks if username is empty
+ 
+    // Check if username is empty
     if(empty(trim($_POST["username"]))){
-        $username_err = "Indtast brugernavn";
-    }
-    else{
+        $username_err = "Please enter username.";
+    } else{
         $username = trim($_POST["username"]);
     }
     
-    // Checks if password is empty
+    // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
-    }
-    else{
+    } else{
         $password = trim($_POST["password"]);
     }
-
-    //Validate credentials
-    if(empty($username_err) && empty ($password_err)){
-        //Prepare select statement
-
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
-
+        
         if($stmt = $mysqli->prepare($sql)){
-            //binding variables to preparedstatement and other voodoo things
-            $stmt->bind_parameter("s", $param_username);
-
-            //Sets username parameter
+            // Bind variables to the prepared statement
+            $stmt->bind_param("s", $param_username);
+            
             $param_username = $username;
-
+            
+            // execute prepared statement
             if($stmt->execute()){
-                //Stores result
+                // Store result
                 $stmt->store_result();
-
-                //check if username exists, if it does then verify password
-                if($stmt-> num_rows == 1){
+                
+                // Check if username exists, if yes then verify password
+                if($stmt->num_rows == 1){                    
+                    // Bind result variables
                     $stmt->bind_result($id, $username, $hashed_password);
                     if($stmt->fetch()){
                         if(password_verify($password, $hashed_password)){
-                            //Password is correct so a new session is started
+                           
+                            
                             session_start();
-
+                            
+                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            //redirects user to overview.php
+                            $_SESSION["username"] = $username;                            
+                            
                             header("location: overview.php");
-                        }
-                        //happens if password is not valid
-                        else{
-                            $login_err = "Brugernavn eller password er forkert.";
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
                         }
                     }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
                 }
-                //happens if user doesnt exist
-                else {
-                    $login_err = "Brugernavn eller password er forkert.";
-                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
             }
-            else
-            {
-                echo "noget er gået galt. Prøv igen.";
-            }
+
+            // Close statement
             $stmt->close();
         }
     }
-    $mysqli->close();
 }
 ?>
 
@@ -89,13 +88,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </head> 
 
     <body>
-
-        <form action="overview.php" method="post" class="loginForm">
-            <?php 
-            ?>
+<div class="wrapper">
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        } 
+        ?>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="loginForm">
                 <label for="uname" style="font-size: 3vh;"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required><br>
-
+                <input type="text" placeholder="Enter Username" name="uname" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <br>
+                
                 <label for="psw" style="font-size: 3vh;"><b>Password </b></label>
                 <input type="password" placeholder="Enter Password" name="psw" required><br>
                 <label style="font-size: 2vh;">
@@ -105,6 +108,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <span class="psw" style="font-size: 2vh;">Forgot <a href="#">password?</a></span>
                 <button type="submit" style="float: right;margin-right: 9vw;margin-top: -3vh;">Login</button><br>
         </form> 
-
+</div>
     </body>
 </html> 
